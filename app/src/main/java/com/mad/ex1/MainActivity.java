84c7d231b0021ac094531320aee77f0d316b8451
Amrit <amrittheengineer@gmail.com>
2021-04-03 14:47:48 +0530

    Ex 3 done.

--- app/src/main/java/com/mad/ex1/MainActivity.java
+++ app/src/main/java/com/mad/ex1/MainActivity.java
@@ -2,7 +2,10 @@ package com.mad.ex1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,37 +13,49 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView textToManipulate;
-    private Button changeSize, changeColor;
-    private int fontSize = 24, colorPos = 0;
-    private final int[] COLORS = new int[]{Color.RED, Color.BLUE, Color.GREEN, Color.MAGENTA, Color.YELLOW, Color.CYAN, Color.BLACK};
+    private static class MyView extends View{
+
+        public MyView(Context context) {
+            super(context);
+        }
+
+        Paint paint = new Paint();
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
+
+            paint.setTextSize(40);
+
+            paint.setColor(Color.GREEN);
+            canvas.drawText("Circle", 55, 30, paint);
+
+            paint.setColor(Color.RED);
+            canvas.drawCircle(100, 150,100, paint);
+
+            paint.setColor(Color.GREEN);
+            canvas.drawText("Rectangle", 255, 30, paint);
+
+            paint.setColor(Color.YELLOW);
+            canvas.drawRect(250, 50,400,350, paint);
+
+            paint.setColor(Color.GREEN);
+            canvas.drawText("SQUARE", 55, 430, paint);
+
+            paint.setColor(Color.BLUE);
+            canvas.drawRect(50, 450,150,550, paint);
+
+            paint.setColor(Color.GREEN);
+            canvas.drawText("LINE", 255, 430, paint);
+
+            paint.setColor(Color.RED);
+            canvas.drawLine(250, 500, 350, 500, paint);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        textToManipulate = findViewById(R.id.textToManipulate);
-        changeSize = findViewById(R.id.changeSize);
-        changeColor = findViewById(R.id.changeColor);
-
-        changeSize.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                fontSize += 4;
-                textToManipulate.setTextSize(fontSize);
-                if (fontSize > 50){
-                    fontSize = 24;
-                }
-            }
-        });
-
-        changeColor.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                textToManipulate.setTextColor(COLORS[colorPos]);
-                colorPos = (colorPos + 1) % COLORS.length;
-            }
-        });
+        setContentView(new MyView(this));
     }
 }
